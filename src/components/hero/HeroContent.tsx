d2442796f210c2c08461e084b4b7ab2f83e81aa0
SRINJOY DAS <srinjoydas1104@gmail.com>
2024-12-09 10:24:00 +0530

    updated

--- src/components/hero/HeroContent.tsx
+++ src/components/hero/HeroContent.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { motion } from 'framer-motion';
 import { Github, Linkedin, Mail, Terminal } from 'lucide-react';
 import { TypewriterEffect } from './TypewriterEffect';
